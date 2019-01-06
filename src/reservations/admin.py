d60984489c19cc9f0a71688f2c21defaf5c0cd29
Marek Makovec <marovi@spadnul.net>
2019-01-06 19:16:18 +0100

    enable court edit in admin

--- src/reservations/admin.py
+++ src/reservations/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Reservation, Sport
+from .models import Court, Reservation, Sport
 
+admin.site.register(Court)
 admin.site.register(Reservation)
 admin.site.register(Sport)
